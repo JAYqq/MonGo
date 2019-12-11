b1c3d34947385ff50a06dec93d7144af13e2212f
Mason <scwscw123@sina.com>
2019-12-11 17:16:26 +0800

    配置mysql

--- back-end/config.py
+++ back-end/config.py
@@ -6,9 +6,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
+    #SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
     #     'sqlite:///'+os.path.join(basedir,"app.db")
-    SQLALCHEMY_DATABASE_URI="mysql://root:123456@localhost:3306/mongoblog?charset=utf8mb4"
+    SQLALCHEMY_DATABASE_URI="mysql://mongo:scw123@localhost:3306/mongoblog?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS =False
     SQLALCHEMY_COMMIT_ON_TEARDOWN=True
     #SQLALCHEMY_ECHO=True
